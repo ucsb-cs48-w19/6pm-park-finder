b97a44cd8f72f306dfe904872923d6e0a53d1218
AndrewJGaut <ajg@umail.ucsb.edu>
2019-02-03 17:45:00 -0800

    Started adding functionality to change scene on button click

---
+++ Assets/LoadParkPage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadParkPage : MonoBehaviour
+{
+    private GameObject wrapper = GameObject.Find("Canvas");
+    private GameObject label = wrapper.GetComponent<GUIText>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        label.text = PlayerPrefs.GetString("PrevPark");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class script : MonoBehaviour
+{
+    public Button Test_Button;
+
+    void Start()
+    {
+        Test_Button.onClick.AddListener(PrintOut);
+    }
+
+    void PrintOut()
+    {
+        Debug.Log("Test_Button clicked");
+        PlayerPrefs.SetString("ParkToLoad", Test_Button.name);
+        SceneManager.LoadScene("ParkPage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
