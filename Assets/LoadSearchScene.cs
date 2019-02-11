d6d37a8767ca9b02787eb05d36e37bdb6205c447
AndrewJGaut <ajg@umail.ucsb.edu>
2019-02-10 20:07:10 -0800

    updated search button so links to search page

---
+++ Assets/LoadSearchScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class LoadSearchScene : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Search_Scene");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
