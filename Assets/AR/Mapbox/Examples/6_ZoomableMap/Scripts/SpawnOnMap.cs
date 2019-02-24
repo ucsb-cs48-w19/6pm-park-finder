bfb6b3417f3632e49f97b52efa240ff6f16c6495
AndrewJGaut <ajg@umail.ucsb.edu>
2019-02-23 22:33:10 -0800

    added functinality to generate pins from querying database

--- Assets/AR/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Assets/AR/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -7,30 +7,38 @@
 	using Mapbox.Unity.Utilities;
 	using System.Collections.Generic;
     using System.IO;
+    using UnityEngine.Networking;
+    using System.Collections;
 
     public class SpawnOnMap : MonoBehaviour
-	{
-		[SerializeField]
-		AbstractMap _map;
+    {
+        [SerializeField]
+        AbstractMap _map;
 
-		[SerializeField]
-		[Geocode]
-		string[] _locationStrings;
-		Vector2d[] _locations;
+        [SerializeField]
+        [Geocode]
+        string[] _locationStrings;
+        Vector2d[] _locations;
 
-		[SerializeField]
-		float _spawnScale = 100f;
+        [SerializeField]
+        float _spawnScale = 100f;
 
-		[SerializeField]
-		GameObject _markerPrefab;
+        [SerializeField]
+        GameObject _markerPrefab;
 
-		List<GameObject> _spawnedObjects;
+        List<GameObject> _spawnedObjects;
 
         string path = "Assets/Resources/park_locs.txt";
+        private static string phpUrl = "https://server-for-parkfinder.000webhostapp.com/get_park_locs.php";
 
 
         void Start()
         {
+            var coroutine = GetLocStrings();
+            StartCoroutine(coroutine);
+
+            //var coroutine = 
+            /*
             //create location strings
             _locationStrings = new string[4];
             StreamReader sr = new System.IO.StreamReader(path);
@@ -41,31 +49,72 @@
                 _locationStrings[j] = line;
                 j++;
             }
-
-
+            */
+            /*
             _locations = new Vector2d[_locationStrings.Length];
-			_spawnedObjects = new List<GameObject>();
-			for (int i = 0; i < _locationStrings.Length; i++)
-			{
-				var locationString = _locationStrings[i];
-				_locations[i] = Conversions.StringToLatLon(locationString);
-				var instance = Instantiate(_markerPrefab);
-				instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
-				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-				_spawnedObjects.Add(instance);
-			}
-		}
+            _spawnedObjects = new List<GameObject>();
+            for (int i = 0; i < _locationStrings.Length; i++)
+            {
+                var locationString = _locationStrings[i];
+                _locations[i] = Conversions.StringToLatLon(locationString);
+                var instance = Instantiate(_markerPrefab);
+                instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+                instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+                _spawnedObjects.Add(instance);
+            }
+            */
+        }
 
-		private void Update()
-		{
-			int count = _spawnedObjects.Count;
-			for (int i = 0; i < count; i++)
-			{
-				var spawnedObject = _spawnedObjects[i];
-				var location = _locations[i];
-				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
-				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
-			}
-		}
+        private IEnumerator GetLocStrings()
+        {
+            var locs = UnityWebRequest.Get(phpUrl);
+            //WWW parkCharacteristics = new WWW(phpUrl, form);
+
+            yield return locs.SendWebRequest();
+            //yield return parkCharacteristics;
+
+
+            if (locs.isNetworkError || locs.isHttpError)
+            {
+                print("Error downloading: " + locs.error);
+                //description = "error";
+                Debug.Log("ERROR");
+            }
+            else
+            {
+                // show the highscores
+                Debug.Log("ABOUT TO PRINT OBJECT");
+                Debug.Log(locs.downloadHandler.text);
+                //description = parkCharacteristics.downloadHandler.text;
+                _locationStrings = locs.downloadHandler.text.Split('\n');
+                _locations = new Vector2d[_locationStrings.Length];
+                _spawnedObjects = new List<GameObject>();
+                for (int i = 0; i < _locationStrings.Length; i++)
+                {
+                    if (_locationStrings[i] == "") continue;
+                    var locationString = _locationStrings[i];
+                    _locations[i] = Conversions.StringToLatLon(locationString);
+                    var instance = Instantiate(_markerPrefab);
+                    instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
+                    instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+                    _spawnedObjects.Add(instance);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            if (_spawnedObjects.Count > 0)
+            {
+                int count = _spawnedObjects.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var spawnedObject = _spawnedObjects[i];
+                    var location = _locations[i];
+                    spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                    spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+                }
+            }
+        }
 	}
 }
