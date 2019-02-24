482ef6c22529807da6b3bd6b84ab9502c2fd16e2
Felix <felixzzz06@gmail.com>
2019-02-24 02:08:02 -0800

    FZ Set latitude and longitude of searchObjects when adding from db

--- Assets/Scripts/SearchBarObject.cs
+++ Assets/Scripts/SearchBarObject.cs
@@ -7,9 +7,21 @@ public class SearchBarObject : MonoBehaviour
 {
 
     public Text nameText;
+	private double[] latLong = new double[2] ;
 
     public void setName()
     {
         nameText.text = this.name;
     }
+
+	public void setLatLong(double lat, double lon) {
+		latLong[0] = lat ;
+		latLong[1] = lon ;
+		/* Debug.Log(latLong[0].ToString("0.000000")) ; */
+		/* Debug.Log(latLong[1].ToString("0.000000")) ; */
+	}
+
+	public double[] getLatLong() {
+		return latLong ;
+	}
 }
