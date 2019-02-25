30ccdbb31c7121c6e967080846093581a6743ecc
Felix <felixzzz06@gmail.com>
2019-02-24 17:24:10 -0800

    FZ Clean some debug statements

--- Assets/Scripts/SearchBarObject.cs
+++ Assets/Scripts/SearchBarObject.cs
@@ -28,8 +28,6 @@ public class SearchBarObject : MonoBehaviour
 	public void setLatLong(double lat, double lon) {
 		latLong[0] = lat ;
 		latLong[1] = lon ;
-		/* Debug.Log(latLong[0].ToString("0.000000")) ; */
-		/* Debug.Log(latLong[1].ToString("0.000000")) ; */
 	}
 
 	public Vector2d getLatLong() {
