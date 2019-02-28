80212cb085a0e56e8cbdff3bd57ab91ff0453872
Felix <felixzzz06@gmail.com>
2019-02-28 00:25:09 -0800

    FZ Merge with master and add unit testing to travis

---
+++ 6pm-park-finder/Assets/Scripts/Tests/Editor/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class Test
+{
+    private static GameObject park;
+
+	[SetUp]
+    public void SetUp()
+    {
+        park = Resources.Load("/Prefabs/Park") as GameObject;
+    }
+
+    [UnityTest]
+    public IEnumerator Tests_correctSearchObject()
+    {
+        SearchableObject objectToTest = new SearchableObject(park);
+
+        // Wait for Start Method
+        yield return null;
+        //
+        Assert.AreEqual(objectToTest.searchObject, park);
+    }
+
+	public static void TestTest() {
+		Test tt = new Test() ;
+		tt.SetUp() ;
+		tt.Tests_correctSearchObject() ;
+	}
+}
