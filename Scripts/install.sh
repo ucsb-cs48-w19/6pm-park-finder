d91235648103a2ab262dc336f42921209036620b
Felix <felixzzz06@gmail.com>
2019-02-27 20:10:32 -0800

    FZ Fix bash.sh downloads missing urls

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -3,12 +3,14 @@
 # BASE_URL=https://netstorage.unity3d.com/unity
 # HASH=88d00a7498cd
 # VERSION=5.5.1f1
+BASE_URL=https://download.unity3d.com/download_unity/cef3e6c0c622/
+HASH=cef3e6c0c622
 VERSION=2018.2.20f1
 # 2.17
 download() {
   file=$1
-  # url="$BASE_URL/$HASH/$package"
-  url="https://download.unity3d.com/download_unity/cef3e6c0c622/MacEditorInstaller/Unity-2018.2.20f1.pkg"
+  url="$BASE_URL/$HASH/$package"
+  # url="https://download.unity3d.com/download_unity/cef3e6c0c622/MacEditorInstaller/Unity-2018.2.20f1.pkg"
 #url="https://download.unity3d.com/download_unity/bb579dc42f1d/UnityDownloadAssistant.dmg"
 
   echo "Downloading from $url: "
@@ -28,6 +30,7 @@ install() {
 # of available packages, where PLATFORM is `osx` or `win`
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+# install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Mono-Support-for-Editor-$VERSION.pkg"
+# install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
