bb85fb2ae855a313977b7d6f8cdd64879a26fb76
Felix <felixzzz06@gmail.com>
2019-02-27 19:37:31 -0800

    FZ Add cat to build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -10,3 +10,4 @@ echo "Attempting to build $project for OS X"
 # /Applications/Unity/Unity.app/Contents/MacOS/Unity -executeMethod Build.PerformBuild -batchmode -logFile $(pwd)/unity.log -nographics -silent-crashes -quit
 # /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile $(pwd)/unity.log -nographics -silent-crashes -projectPath "$/Users/travis/build/ucsb-cs48-w19/6pm-park-finder/${project}" -buildOSXUniversalPlayer "$(pwd)/Build/osx/${project}.app" \ -quit
 /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "/Users/travis/build/ucsb-cs48-w19/6pm-park-finder/6pm-park-finder" -executeMethod Build.PerformBuild -quit
+cat $(pwd)/unity.log
