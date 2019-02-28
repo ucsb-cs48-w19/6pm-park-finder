80df2825107b0124205cc02ce3ea7972764c9d13
Felix <felixzzz06@gmail.com>
2019-02-27 19:14:08 -0800

    FZ Debug

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -9,5 +9,5 @@ export EVENT_NOKQUEUE=1
 echo "Attempting to build $project for OS X"
 # /Applications/Unity/Unity.app/Contents/MacOS/Unity -executeMethod Build.PerformBuild -batchmode -logFile $(pwd)/unity.log -nographics -silent-crashes -quit
 # /Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile $(pwd)/unity.log -nographics -silent-crashes -projectPath "$/Users/travis/build/ucsb-cs48-w19/6pm-park-finder/${project}" -buildOSXUniversalPlayer "$(pwd)/Build/osx/${project}.app" \ -quit
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile $(pwd)/unity.log -nographics -silent-crashes -projectPath "/Users/travis/build/ucsb-cs48-w19/6pm-park-finder/${project}" -executeMethod Build.PerformBuild \ -quit
-
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $(pwd)/unity.log -projectPath "/Users/travis/build/ucsb-cs48-w19/6pm-park-finder/6pm-park-finder" -executeMethod Build.PerformBuild -quit
+cat $(pwd)/unity.log
